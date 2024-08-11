5132f66d0a2b1bad16cd6d541f5dd8229f9be7e4
Bryan Elmer <bryan.elmer.mulijono@gmail.com>
2024-08-12 00:41:11 +0800

    add styles for navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,12 +2,16 @@
 
 import Link from "next/link"
 import styles from "./index.module.css"
+import { usePathname } from "next/navigation";
+
 import { useLogout } from "@/hooks/useLogout"
 import { useAuthContext } from "@/hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout()
   const { state } = useAuthContext()
+  const pathName = usePathname()
+  console.log(pathName === '/login')
 
   const handleClick = () => {
     logout()
@@ -17,7 +21,7 @@ const Navbar = () => {
     <header className={styles.navbar}>
       <div className={styles.container}>
         <Link href={"/"}>
-          <h1 className="font-bold">Workout Buddy</h1>
+          <h1 className="font-bold">Gym Buddy</h1>
         </Link>
         <nav className={styles.nav}>
           {state.user && (
@@ -28,8 +32,8 @@ const Navbar = () => {
           )}
           {!state.user && (
             <div>
-              <Link href={"/login"}>Login</Link>
-              <Link href={"/signup"}>Signup</Link>
+              <Link href={"/login"} className={pathName === "/login" ? styles.activeLink : ""}>Login</Link>
+              <Link href={"/signup"} className={pathName === "/signup" ? styles.activeLink : ""}>Signup</Link>
             </div>
           )}
         </nav>
